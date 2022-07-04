fb30f33c4407d064a0814027e6ec6fd0dc62a3c6
Rauan <abishevrauan@gmail.com>
2022-07-04 12:22:35 +0600

    www

--- employees/serializers.py
+++ employees/serializers.py
@@ -6,4 +6,4 @@ from .models import Employee
 class EmployeeSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Employee
-        fields = ('last_name', 'first_name', 'middle_name')
+        fields = ('last_name', 'first_name', 'middle_name', 'chief')
